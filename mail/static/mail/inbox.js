866c5a03690289e1dcbfb3a3995a5cdb05e9a84b
han-le <58657916+han-le@users.noreply.github.com>
2021-08-07 12:25:29 +0700

    fixing the load mailbox after archived

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,15 +1,15 @@
 // ------*****------- CALL functions here ------*****-------
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
-  // Use buttons to toggle between views
-  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
-  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
-  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
-  document.querySelector('#compose').addEventListener('click', compose_email);
+    // Use buttons to toggle between views
+    document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+    document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+    document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+    document.querySelector('#compose').addEventListener('click', compose_email);
 
-  // By default, load the inbox
-  load_mailbox('inbox');
+    // By default, load the inbox
+    load_mailbox('inbox');
 
 });
 
@@ -35,33 +35,34 @@ function compose_email() {
 
 function load_mailbox(mailbox) {
 
-  let API_url = `emails/${mailbox}`
-  fetch(API_url)
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
+    let API_url = `emails/${mailbox}`
+    fetch(API_url)
+        .then(
+            function (response) {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' +
+                        response.status);
+                    return;
+                }
 
-      // Examine the text in the response
-      response.json().then(data => {
-          // When the data is NOT empty
-          if (data.length > 0) {
-            showEmails(mailbox, data)
-          } else {
-            // When the data is empty
-            document.querySelector('#emails-view').innerHTML = `<h3>This is empty</h3>`
-          }
-          console.log(mailbox, 'are loaded')
-      });
-    }
-  )
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
-  
+                // Examine the text in the response
+                response.json().then(data => {
+                    // When the data is NOT empty
+                    if (data.length > 0) {
+                        showEmails(mailbox, data);
+                        console.log(data)
+                    } else {
+                        // When the data is empty
+                        document.querySelector('#emails-view').innerHTML = `<h3>This is empty</h3>`
+                    }
+                    console.log(mailbox, 'are loaded')
+                });
+            }
+        )
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+        });
+
     // Show the mailbox and hide other views
     document.querySelector('#emails-view').style.display = 'block';
     document.querySelector('#compose-view').style.display = 'none';
@@ -74,10 +75,10 @@ const showEmails = (mailbox, email_list) => {
     if (mailbox === 'sent') {
         // Do not add archive btn
         email_list.forEach((email) => {
-        let emailContainer = document.createElement('div');
-        emailContainer.innerHTML = `
+            let emailContainer = document.createElement('div');
+            emailContainer.innerHTML = `
             <div class="" style="background: lightyellow">
-                <li>Email id: ${email.id}</li>
+                <h5>Email id: ${email.id}</h5>
                 <li>Read status: ${email.read}</li>
                 <li>Archived status: ${email.archived}</li>
                 <li>From: ${email.sender}</li>
@@ -87,21 +88,23 @@ const showEmails = (mailbox, email_list) => {
             </div>
             <hr>
         `;
-        // Add event show Email to email div
-        emailContainer.addEventListener('click', () => {showEmail(`${email.id}`, inSent='true')});
+            // Add event show Email to email div
+            emailContainer.addEventListener('click', () => {
+                showEmail(`${email.id}`, inSent = 'true')
+            });
 
-        // Add event archive to archive btn
-        emailWrapper.append(emailContainer);
-    });
+            // Add event archive to archive btn
+            emailWrapper.append(emailContainer);
+        });
     } else {
         // Add archive btn
         email_list.forEach((email) => {
-        let emailContainer = document.createElement('div');
-        emailContainer.innerHTML = `
+            let emailContainer = document.createElement('div');
+            emailContainer.innerHTML = `
             <div class="" style="background: lightyellow">
-                <li>Email id: ${email.id}</li>
-                <li>Read status: ${email.read}</li>
-                <li>Archived status: ${email.archived}</li>
+                <h5>Email id: ${email.id}</h5>
+                <li><strong>Read status: ${email.read}</strong></li>
+                <li><strong>Archived status: ${email.archived}</strong></li>
                 <li>From: ${email.sender}</li>
                 <li>To: ${email.recipients}</li>
                 <li>Subject: ${email.subject}</li>
@@ -110,12 +113,14 @@ const showEmails = (mailbox, email_list) => {
             <button onclick="archiveEmail(${email.id}, ${email.archived}, event)">Archive</button>
             <hr>
         `;
-        // Add event show Email to email div
-        emailContainer.addEventListener('click', () => {showEmail(`${email.id}`, inSent='false')});
+            // Add event show Email to email div
+            emailContainer.addEventListener('click', () => {
+                showEmail(`${email.id}`, inSent = 'false')
+            });
 
-        // Add event archive to archive btn
-        emailWrapper.append(emailContainer);
-    });
+            // Add event archive to archive btn
+            emailWrapper.append(emailContainer);
+        });
     }
 
     document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
@@ -163,22 +168,22 @@ const showEmail = (email_id, inSent) => {
     let url = `/emails/${email_id}`;
     fetch(url)
         .then((response) => {
-			if (response.status !== 200) {
-            console.log('Status code: ' + response.status);
-            return;
-            }
-            // Success
-            response.json().then((emailObj) => {
-                console.log(emailObj);
-                // Render the full email
-                renderEmailView(emailObj, inSent);
-                // Mark the email as read/seen
-                if (emailObj.read === false) {
-                    markAsRead(emailObj);
+                if (response.status !== 200) {
+                    console.log('Status code: ' + response.status);
+                    return;
                 }
-            });
-        }
-    ).catch((err) => {
+                // Success
+                response.json().then((emailObj) => {
+                    console.log(emailObj);
+                    // Render the full email
+                    renderEmailView(emailObj, inSent);
+                    // Mark the email as read/seen
+                    if (emailObj.read === false) {
+                        markAsRead(emailObj);
+                    }
+                });
+            }
+        ).catch((err) => {
         console.log('Fetch Error :-S', err);
     });
 }
@@ -186,7 +191,7 @@ const showEmail = (email_id, inSent) => {
 const renderEmailView = (email, inSent) => {
     let emailView = document.querySelector('#email-content-view');
     if (inSent === true) {
-        emailView.innerHTML= `<div class="">
+        emailView.innerHTML = `<div class="">
                 <div>email id: ${email.id}</div>
                 <div>From: ${email.sender}</div>
                 <div>To: ${email.recipients}</div>
@@ -195,7 +200,7 @@ const renderEmailView = (email, inSent) => {
                 <div>${email.body}</div>
             </div>`;
     } else {
-        emailView.innerHTML= `<div class="">
+        emailView.innerHTML = `<div class="">
                 <div>email id: ${email.id}</div>
                 <div>From: ${email.sender}</div>
                 <div>To: ${email.recipients}</div>
@@ -219,37 +224,38 @@ const markAsRead = (email) => {
     let url = `emails/${email.id}`;
 
     fetch(url, {
-      body: JSON.stringify({
+        body: JSON.stringify({
             read: true
-      }),
+        }),
     })
-    .then(data => {
-        console.log('Success: Marked as read', data);
-    })
-    .catch((error) => {
-      console.log('Error:', error);
-    });
+        .then(data => {
+            console.log('Success: Marked as read', data);
+        })
+        .catch((error) => {
+            console.log('Error:', error);
+        });
 }
 
 const archiveEmail = (email_id, archived_status, event) => {
     // Stop calling the event above it
     event.stopPropagation();
-    let url = 'emails/' + email_id;
 
+    let url = 'emails/' + email_id;
     fetch(url, {
-      method: 'PUT',
-      body: JSON.stringify({
-          archived: archived_status !== true
-      })
+        method: 'PUT',
+        body: JSON.stringify({
+            archived: archived_status !== true
+        })
     })
-    .then(response => {
-        console.log('Marked/Unmarked as archived Success:', response);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-    // console.log(mailbox)
-    load_mailbox('inbox');
+        .then(response => {
+            console.log('Marked/Unmarked as archived Success:', response);
+        })
+        .then(() => {
+            load_mailbox('inbox');
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        })
 }
 
 const test = (emailID, status) => {
