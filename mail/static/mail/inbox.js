1f797eaaba2f43df0a822d67f8c66e5d8175ccec
han-le <58657916+han-le@users.noreply.github.com>
2021-08-09 01:08:56 +0700

    Autofocus for Modal

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -325,20 +325,30 @@ const archiveEmail = (email) => {
 }
 
 const reply = (email) => {
+    console.log('Calling function REPLY')
     // Pre-fill recipient, subject and body
     let subject = email.subject;
     // Add Re: to subject
     if (email.subject.substring(0, 3).toLowerCase() !== 're:') {
         subject = 'Re: ' + email.subject;
     }
-
+    // Add old conversations
     let originalMessage = '\n-----------------------\n' + `On ${email.timestamp} ${email.sender} wrote:` + '\n' + email.body;
 
     document.querySelector('#compose-recipients').value = email.sender;
     document.querySelector('#compose-subject').value = subject;
     document.querySelector('#compose-body').value = originalMessage;
 
-    console.log('This loads when you click on REPLY button');
+    // Autofocus on email body
+    let myModal = document.getElementById('mailModal');
+    let myInput = document.getElementById('compose-body');
+
+    myModal.addEventListener('shown.bs.modal', function () {
+        console.log('autofocus works')
+        myInput.focus();
+        myInput.setSelectionRange(0,0); //start at the beginning
+    })
+
     sendEmail();
 }
 
